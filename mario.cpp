a388e75929fc3743a3abe3307409c0950b201c40
Paliy Igop <Igop2828@gmail.com>
2020-04-27 21:54:48 +0300

    Сreate the second method of MY TREE

--- mario.cpp
+++ mario.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void ShowTreeOne(const int height);
+void ShowTreeTwo(const int height);
 
 int main()
 {
@@ -11,7 +12,7 @@ int main()
 
 	cout << "Enter height pyramid. Max = " << NUMBER_TEN << endl;
 
-	while (true)
+	while (true)											//Cycle verifies correct input
 	{
 		cin >> height;
 		if (!cin || height > NUMBER_TEN || height <= 0)
@@ -23,10 +24,10 @@ int main()
 		else
 			break;
 	}
-	cout << "Height = " << height << endl;
+	cout << "Height = " << height << endl;					// Show to console Height
 
 	cout << "Enter option pyramid. Only 1 or 2\n";
-	while (true)
+	while (true)											//Cycle verifies correct input
 	{
 		cin >> option;
 		if (option == 1 || option == 2)
@@ -36,22 +37,47 @@ int main()
 		cin.clear();
 		while (cin.get() != '\n');
 	}
-	cout << "Option = " << option << endl;
+	cout << "Option = " << option << endl;					// Show to console Option
 	
 	cout << "Your pyramid\n";
-	if (option == 1)
+	if (option == 1)										// My first method of MY TREE
 		ShowTreeOne(height);
+	else if (option == 2)									// My second method of MY TREE
+		ShowTreeTwo(height);
 
-	return 0;
+	return 0;												// End my program
 }
 
-void ShowTreeOne(const int height)
+void ShowTreeOne(const int height)							//First function TREE
 {
 	for (int i = 0; i < height; i++)
 	{
 		for (int j = 0; j < height; j++)
 		{
 			if (i + j >= height - 1)
+				cout << "#";
+			else
+				cout << " ";
+		}
+		cout << endl;
+	}
+}
+
+void ShowTreeTwo(const int height)							//Second function TREE
+{
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			if (i + j >= height - 1)
+				cout << "#";
+			else
+				cout << " ";
+		}
+		cout << " ";
+		for (int j = 0; j < height; j++)
+		{
+			if (i >= j)
 				cout << "#";
 			else
 				cout << " ";
